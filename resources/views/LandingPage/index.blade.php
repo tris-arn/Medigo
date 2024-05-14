e6f677e1d930c51ed1dac7669a2149abecda4ccd
tristan <tristanaaron.2711@gmail.com>
2024-05-14 09:40:11 +0700

    Add Image to landing

--- resources/views/LandingPage/index.blade.php
+++ resources/views/LandingPage/index.blade.php
@@ -2,73 +2,73 @@
 
 @section('container')
     <img src="{{ asset("image\BannerMain.png") }}" style="width:100%;height:auto;"alt="">
-    <div style="margin-top: 100px">
-        <h1 class="text-center" style="font-size: 40px;font-weight:600" id="chopin">Categories</h1>
+    <div style="margin-top: 150px; width:100%">
+        <h1 class="text-center" style="font-size: 40px;font-weight:600;" id="chopin">Categories</h1>
 
-        <section class="mt-5">
-            <div class="col">
-                <div class="row text-center d-flex justify-content-center" style="width: 100%">
+        <section class="mt-10" style="margin-top: 100px">
+            <div class="col" style=" width:100%" >
+                <div class="row text-center d-flex justify-content-center m-0 " style="width: 100%;margin-bottom:100px">
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Headache.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems" style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">About us</a>
+                        <a href="/" class="text-black text-decoration-none">Headache</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Vitamin.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems" style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">Call Us</a>
+                        <a href="/" class="text-black text-decoration-none">Vitamin</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Ingestion.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems" style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">Promo</a>
+                        <a href="/" class="text-black text-decoration-none">Ingestion</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Allergy.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems" style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">FAQ</a>
+                        <a href="/" class="text-black text-decoration-none">Allergy</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\NeckPain.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems" style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">Terms and Conditions</a>
+                        <a href="/" class="text-black text-decoration-none">Neck Pain</a>
                         </h6>
                     </div>
                 </div>
             
-                <div class="row text-center d-flex justify-content-center" style="margin-top: 50px;width:100%">
-                    <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                <div class="row text-center d-flex justify-content-center m-0" style="margin-top: 100px;width:100%">
+                    <div id="mg" class="col-md-2 mb-3 mt-0">
+                        <img id="cateimg" src="image\Liver.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems"  style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">About us</a>
+                        <a href="/" class="text-black text-decoration-none">Liver</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Hearing.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems"  style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">Call Us</a>
+                        <a href="/" class="text-black text-decoration-none">Hearing</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Dental.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems"  style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">Promo</a>
+                        <a href="/" class="text-black text-decoration-none">Dental</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Flu.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems"  style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">FAQ</a>
+                        <a href="/" class="text-black text-decoration-none">Flu</a>
                         </h6>
                     </div>
                     <div class="col-md-2 mb-3 mt-0">
-                        <img id="cateimg" src="" alt="">
+                        <img id="cateimg" src="image\Eyesight.png" alt="">
                         <h6 class="font-weight-bold" id="NavItems" style="margin-top: 10px">
-                        <a href="/" class="text-black text-decoration-none">Terms and Conditions</a>
+                        <a href="/" class="text-black text-decoration-none">Eyesight</a>
                         </h6>
                     </div>
                 </div>
@@ -76,9 +76,9 @@
         </section>
     </div>
 
-    <div style="margin-top:115px">
-        <div class="row text-center d-flex justify-content-around" style="margin-top: 50px;width:100%">
-            <div id="PromoBanner" class="col-md-2 mb-3 mt-0">
+    <div style="margin-top:150px; width:100%">
+        <div class="row text-center d-flex justify-content-evenly m-0" style="margin-top: 50px;width:100%">
+            <div id="PromoBanner" class="col-md-2 mb-3 mt-0" style="background-image: url('/image/Promo1.png');">
                 <div style="height: 100%;display: flex; align-items: flex-end; justify-content:center;">
                     <div style="margin-bottom: 20px">
                         <h5 class="font-weight-bold" id="NavItems" style="margin-bottom: 0px">
@@ -88,7 +88,7 @@
                     </div>
                 </div>
             </div>
-            <div id="PromoBanner" class="col-md-2 mb-3 mt-0">
+            <div id="PromoBanner" class="col-md-2 mb-3 mt-0" style="background-image: url('/image/Promo2.png');">
                 <div style="height: 100%;display: flex; align-items: flex-end; justify-content:center;">
                     <div style="margin-bottom: 20px">
                         <h5 class="font-weight-bold" id="NavItems" style="margin-bottom: 0px">
@@ -98,7 +98,7 @@
                     </div>
                 </div>
             </div>
-            <div id="PromoBanner" class="col-md-2 mb-3 mt-0">
+            <div id="PromoBanner" class="col-md-2 mb-3 mt-0" style="background-image: url('/image/Promo3.png');">
                 <div style="height: 100%;display: flex; align-items: flex-end; justify-content:center;">
                     <div style="margin-bottom: 20px">
                         <h5 class="font-weight-bold" id="NavItems" style="margin-bottom: 0px">
@@ -116,10 +116,10 @@
 
         <input type="radio" class="tabs__radio" name="tabs-div" id="Best_Seller" checked>
         <label for="Best_Seller" class="tabs__label">Best</label>
-        <div class="tabs__content">
-              <div class="row text-center d-flex justify-content-center" style="width: 100%">
+        <div class="tabs__content" style="width:100%">
+              <div class="row text-center d-flex justify-content-center m-0" style="width: 100%">
                     <div class="col-md-1  card">
-                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
+                        <img src="{{ asset("image/BannerMain.png") }}" class="crd-img" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <label class="card-desc">Per Strip</label>
@@ -168,50 +168,55 @@
         <input type="radio" class="tabs__radio" name="tabs-div" id="sale">
         <label for="sale" class="tabs__label">Sale</label>
         <div class="tabs__content">
-            <div class="row text-center d-flex justify-content-center" style="width: 100%">
-                    <div class="col-md-1  card">
-                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <label class="card-desc">Per Strip</label>
-                            <h5 class="card-title">Rp 70.000</h5>
-                            <a href="#" id="addbtn" class="btn">ADD</a>
-                        </div>
-                    </div>
-                    <div class="card col-md-1">
-                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <label class="card-desc">Per Strip</label>
-                            <h5 class="card-title">Rp 70.000</h5>
-                            <a href="#" id="addbtn" class="btn">ADD</a>
-                        </div>
-                    </div>
-                    <div class="card col-md-1">
-                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <label class="card-desc">Per Strip</label>
-                            <h5 class="card-title">Rp 70.000</h5>                
-                            <a href="#" id="addbtn" class="btn">ADD</a>
-                        </div>
-                    </div>
-                    <div class="card col-md-1">
-                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <label class="card-desc">Per Strip</label>
-                            <h5 class="card-title">Rp 70.000</h5>
-                            <a href="#" id="addbtn" class="btn">ADD</a>
-                        </div>
-                    </div>
-                    <div class="card col-md-1">
+            <div class="row text-center d-flex justify-content-center m-0" style="width: 100%">
+                    <div class="card col-md-1" style="width: 420px">
                         <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Botak</h5>
                             <label class="card-desc">Per Strip</label>
-                            <h5 class="card-title">Rp 70.000</h5>
-                            <a href="#" id="addbtn" class="btn">ADD</a>
+                            <h5 class="card-title" style="color: red">Rp 70.000</h5>
+                            <label class="card-desc" style="text-decoration-line:line-through;">Rp 100.000</label>
+                            <a href="#" id="addbtn" style="margin-top: 20px" class="btn">ADD</a>
+                        </div>
+                    </div>
+                    <div class="card col-md-1" style="width: 420px">
+                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Botak</h5>
+                            <label class="card-desc">Per Strip</label>
+                            <h5 class="card-title" style="color: red">Rp 70.000</h5>
+                            <label class="card-desc" style="text-decoration-line:line-through;">Rp 100.000</label>
+                            <a href="#" id="addbtn" style="margin-top: 20px" class="btn">ADD</a>
+                        </div>
+                    </div>
+                    <div class="card col-md-1" style="width: 420px">
+                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Botak</h5>
+                            <label class="card-desc">Per Strip</label>
+                            <h5 class="card-title" style="color: red">Rp 70.000</h5>
+                            <label class="card-desc" style="text-decoration-line:line-through;">Rp 100.000</label>
+                            <a href="#" id="addbtn" style="margin-top: 20px" class="btn">ADD</a>
+                        </div>
+                    </div>
+                    <div class="card col-md-1" style="width: 420px">
+                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Botak</h5>
+                            <label class="card-desc">Per Strip</label>
+                            <h5 class="card-title" style="color: red">Rp 70.000</h5>
+                            <label class="card-desc" style="text-decoration-line:line-through;">Rp 100.000</label>
+                            <a href="#" id="addbtn" style="margin-top: 20px" class="btn">ADD</a>
+                        </div>
+                    </div>
+                    <div class="card col-md-1" style="width: 420px">
+                        <img src="{{ asset("image/Drug.png") }}" class="crd-img" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Botak</h5>
+                            <label class="card-desc">Per Strip</label>
+                            <h5 class="card-title" style="color: red">Rp 70.000</h5>
+                            <label class="card-desc" style="text-decoration-line:line-through;">Rp 100.000</label>
+                            <a href="#" id="addbtn" style="margin-top: 20px" class="btn">ADD</a>
                         </div>
                     </div>
                 </div>
